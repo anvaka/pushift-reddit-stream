e5239ded2b8857d34435bbd28148702b9ac388a1
Andrei Kashcha <anvaka@gmail.com>
2020-11-25 18:26:41 -0800

    naive error handling

--- collect.sh
+++ collect.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+while true
+do
 curl --verbose --compressed 'http://stream.pushshift.io/?type=comments' | node process.js data
+sleep 1
+done
--- process.js
+++ process.js
@@ -17,7 +17,18 @@ const resourceManger = new ResourceManager();
 
 rl.on('line', function(line){
   if (line.startsWith('data: ')) {
-    let comment = JSON.parse(line.substring(6));
+    let comment;
+    try {
+      comment = JSON.parse(line.substring(6));
+    } catch(e) {
+      console.error('Failed to parse:');
+      console.error('');
+      console.error(line);
+      console.error('Likely the stream is broken. You need to restart');
+
+      process.exit(0);
+      return;
+    }
     if (comment.created_utc === undefined) return;
     if (comment.author === undefined) throw new Error('Missing author in ' + line);
     if (comment.subreddit === undefined) throw new Error('Missing subreddit in ' + line);
