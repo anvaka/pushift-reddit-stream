4f91f9697bdaf5ebe3b9153f4aa4228026121e3c
Andrei Kashcha <anvaka@gmail.com>
2020-12-11 19:33:02 -0800

    trying to restart

--- collect.sh
+++ collect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true
 do
-curl --verbose --compressed 'http://stream.pushshift.io/?type=comments' | node process.js data
+curl --verbose --compressed -L --retry 999 --retry-max-time 0 'http://stream.pushshift.io/?type=comments' | node process.js data
 sleep 1
 done
