e53f9508628a2855f52ac853f0664a80aa30e44a
Andrei Kashcha <anvaka@gmail.com>
2020-12-23 18:01:42 -0800

    added filtering

--- 01.sort.sh
+++ 01.sort.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-LC_ALL=C sort -u ./data/* > sorted.txt
+LC_ALL=C sort -u ./temp/* > sorted.txt
---
+++ 03.filter.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cat sorted.txt | node filter > filtered.txt
+
+echo "Saved filtered output into filtered.txt"
