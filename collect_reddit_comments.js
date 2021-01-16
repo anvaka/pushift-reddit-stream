20c19f2e9e605026aaf49663a5ab8f3b394492ec
Andrei Kashcha <anvaka@gmail.com>
2021-01-15 20:44:53 -0800

    Store output folder into progress file too

--- collect_reddit_comments.js
+++ collect_reddit_comments.js
@@ -1,21 +1,49 @@
+const fs = require('fs');
 const ResourceManager = require('./ResourceManager');
 const resourceManger = new ResourceManager();
-const outFolder = process.argv[2] || './';
+// const outFolder = process.argv[2] || './';
 const path = require('path');
 
-//let startFrom = 34213292376; // Number.parseInt('fptplco', 36);
-let startFrom = 34302560776;//34233588776;
 const IDS_PER_CALL = 100;
-startFrom = Number.parseInt(process.argv[3]);
-if (!Number.isFinite(startFrom)) {
-  console.error('Pass a decimal id of the comment to indicate the first required post')
+let inputFileName = process.argv[2];
+
+if (!fs.existsSync(inputFileName)) {
+  console.error('Pass a file name where start end rage are defined')
   console.error('This will collect all comments for the day of Dec 4, 2020 and store into "backfill" folder')
-  console.error('  node collect_reddit_comments.js backfill 35707973867 35714381239 ')
-  process.exit(-1);
+  console.error('  echo 35707973867 35714381239 > december.txt')
+  console.error('  node collect_reddit_comments.js backfill december.txt')
+  process.exit(0);
 }
-let endAt = Number.parseInt(process.argv[4]);
+
+let input = fs.readFileSync(inputFileName, 'utf8').split(' ');
+// .map(x => Number.parseFloat(x)).filter(x => Number.isFinite(x));
+if (input.length !== 3) {
+  console.error('Could not parse the input file - values should be: `out_folder startFrom endAt`. E.g.:')
+  console.error('  echo december 35707973867 35714381239 > december.txt')
+  process.exit(0);
+}
+let [outFolder, startFrom, endAt] = input
+startFrom = Number.parseFloat(startFrom);
+if (!Number.isFinite(startFrom)) {
+  console.error('`startFrom` is supposed to be a finite number: ')
+  console.error('  echo december 35707973867 35714381239 > december.txt')
+  process.exit(0);
+}
+endAt = Number.parseFloat(endAt);
 if (!Number.isFinite(endAt)) {
-  endAt = Infinity;
+  console.error('`endAt` is supposed to be a finite number: ')
+  console.error('  echo december 35707973867 35714381239 > december.txt')
+  process.exit(0);
+}
+
+if (startFrom >= endAt) {
+  console.error('Looks like we are done already. Start is larger than end');
+  process.exit(0);
+}
+
+if (!fs.existsSync(outFolder)) {
+  console.log(outFolder + ' does not exist yet. Creating...');
+  fs.mkdirSync(outFolder);
 }
 
 console.log(`Crawlig ${startFrom} to ${endAt} into ${outFolder}`);
@@ -45,7 +73,8 @@ function startTheCrawl() {
     const message = (new Date()).toISOString() + ' Start from: ' + startFrom + '. Last processeed date: ' + getCommentDate(lastComment) + 
     ' wait: ' + response.waitTillNextCall;
     console.log(message);
-    console.warn(message);
+    // console.warn(message);
+    fs.writeFileSync(inputFileName, `${outFolder} ${startFrom} ${endAt}`, 'utf8')
     setTimeout(startTheCrawl, response.waitTillNextCall);
   });
 }
